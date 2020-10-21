3a965a4be6ad06ab93604acb53e7d3b939d8c38b
Maycie Morris <maycie.morris@gmail.com>
2020-10-20 21:38:02 -0400

    edited pics

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -6,6 +6,10 @@ import 'react-awesome-slider/dist/styles.css'
 import testbanner from '../../images/testbanner.png'
 import image1 from '../../images/im1.png'
 import image2 from '../../images/im2.png'
+import image3 from '../../images/im3.png'
+import image4 from '../../images/im4.png'
+import image5 from '../../images/im5.png'
+import image6 from '../../images/im6.png'
 
 
 export const Home = () => {
@@ -22,6 +26,10 @@ export const Home = () => {
                 <div data-src={ testbanner } />
                 <div data-src={ image1 } />
                 <div data-src={ image2 } />
+                <div data-src={ image3 } />
+                <div data-src={ image4 } />
+                <div data-src={ image5 } />
+                <div data-src={ image6 } />
             </AutoplaySlider>
         </div>
             <h1 className='homeHeader'>Custom locally made products.</h1>
